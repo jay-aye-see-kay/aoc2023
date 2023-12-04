2ccd68dbe1bf784dba4d56b747e25215f2f1f0aa
Jack Rose <user@jackrose.co.nz>
2023-12-04 18:27:24 +1100

    day 4 part 2

--- src/day04.py
+++ src/day04.py
@@ -7,6 +7,7 @@ class Card:
     id: int
     winning_nums: list[int]
     have_nums: list[int]
+    copies_won: int
 
     def win_count(self):
         wins = set(self.winning_nums).intersection(set(self.have_nums))
@@ -20,20 +21,27 @@ class Card:
             return 2 ** (self.win_count() - 1)
 
 
+def clean_spaces(input: str):
+    return re.sub(" +", " ", input.strip())
+
+
 def parse_nums(nums: str) -> list[int]:
-    cleaned = re.sub(" +", " ", nums.strip())
-    return [int(n) for n in cleaned.split(" ") if n != ""]
+    return [int(n) for n in clean_spaces(nums).split(" ")]
 
 
 def parse_input(input: str) -> list[Card]:
     cards: list[Card] = []
     for line in input.splitlines():
         s1, s2 = line.split(":")
-        s3, s4 = s2.strip().split("|")
-        id = int(re.sub(" +", " ", s1).split(" ")[1])
-        winning_nums = parse_nums(s3)
-        have_nums = parse_nums(s4)
-        cards.append(Card(id, winning_nums, have_nums))
+        s3, s4 = clean_spaces(s2).split("|")
+        cards.append(
+            Card(
+                id=int(clean_spaces(s1).split(" ")[1]),
+                winning_nums=parse_nums(s3),
+                have_nums=parse_nums(s4),
+                copies_won=1,
+            )
+        )
     return cards
 
 
@@ -42,4 +50,14 @@ def part1(input: str):
     total_points = 0
     for card in cards:
         total_points += card.points()
+    return total_points
+
+
+def part2(input: str):
+    total_points = 0
+    cards = parse_input(input)
+    for i, card in enumerate(cards):
+        total_points += card.copies_won
+        for point in range(card.win_count()):
+            cards[i + point + 1].copies_won += card.copies_won
     return total_points
--- src/test_day04.py
+++ src/test_day04.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.day04 import parse_input, part1
+from src.day04 import parse_input, part1, part2
 
 sample_input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
@@ -14,13 +14,22 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 class TestDay4(unittest.TestCase):
     def test_parse_input(self):
         cards = parse_input(sample_input)
-        total_points = 0
-        for card in cards:
-            total_points += card.points()
         self.assertEqual(len(cards), 6)
-        self.assertEqual(total_points, 13)
+
+    def test_part1_sample(self):
+        count = part1(sample_input)
+        self.assertEqual(count, 13)
 
     def test_part1_real(self):
         with open("./inputs/day-04.txt") as f:
             input = f.read()
         self.assertEqual(part1(input), 22674)
+
+    def test_part2_sample(self):
+        count = part2(sample_input)
+        self.assertEqual(count, 30)
+
+    def test_part2_real(self):
+        with open("./inputs/day-04.txt") as f:
+            input = f.read()
+        self.assertEqual(part2(input), 5747443)
