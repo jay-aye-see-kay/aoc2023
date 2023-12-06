69987a14d53d79b77f248212657ef2aaeb12fa48
Jack Rose <user@jackrose.co.nz>
2023-12-06 21:18:06 +1100

    speed up 6-2 with messy binary search
    - 4.5 sec down to 16ms

--- src/day06.py
+++ src/day06.py
@@ -31,9 +31,46 @@ def part1(input: str):
 
 def part2(input: str):
     time, min_dist = parse_input_2(input)
-    winning_races = []
-    for hold_time in range(1, time):
-        dist = hold_time * (time - hold_time)
-        if dist > min_dist:
-            winning_races.append(dist)
-    return len(winning_races)
+    lowest = binarySearch(range(0, time // 2), min_dist, time)
+    highest = binarySearchRev(range(time // 2, time - 1), min_dist, time)
+    return highest - lowest
+
+
+def binarySearchRev(range_: range, min_dist: int, total_time: int) -> int:
+    first = range_.start
+    last = range_.stop
+    found = False
+
+    while first <= last and not found:
+        pos = 0
+        hold_time = (first + last) // 2
+        dist = hold_time * (total_time - hold_time)
+        if dist == min_dist:
+            pos = hold_time
+            found = True
+        else:
+            if min_dist > dist:
+                last = hold_time - 1
+            else:
+                first = hold_time + 1
+    return pos or hold_time
+
+
+def binarySearch(range_: range, min_dist: int, total_time: int) -> int:
+    first = range_.start
+    last = range_.stop
+    found = False
+
+    while first <= last and not found:
+        pos = 0
+        hold_time = (first + last) // 2
+        dist = hold_time * (total_time - hold_time)
+        if dist == min_dist:
+            pos = hold_time
+            found = True
+        else:
+            if min_dist < dist:
+                last = hold_time - 1
+            else:
+                first = hold_time + 1
+    return pos or hold_time
